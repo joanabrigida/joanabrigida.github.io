9f4d54c53e5457fa6f2e2aa7458445db7d14e7a7
joanabrigida <128098216+joanabrigida@users.noreply.github.com>
2023-05-26 15:12:12 +0100

    java

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/*alert("bibopbupbibbopbbup");
+
+console.log(js);
+
+for (var 1 = 1; i < 30; i++) {
+
+    console.log('Exercício 1 Repetição' + 1 + 'O')
+}*/
+
+
+var anchor = document.querySelector("#main-navigation").querySelectorAll("a");
+
+for (var i = 0; i < anchor.length; i++) {
+  if(window.location.href === anchor[i].href) {
+    anchor[i].classList.add("active"); 
+  }
+
+  if (window.location.href.indexOf("portfolio") > -1) {
+    document.body.classList.add("portfolio");
+  } else if (window.location.href.indexOf("contacts") > -1) {
+    document.body.classList.add("contacts");
+  } else {
+    document.body.classList.add("home");
+  }
+
+}
