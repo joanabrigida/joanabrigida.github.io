02097855367be14a506b6f8512db9a4efe068594
joanabrigida <128098216+joanabrigida@users.noreply.github.com>
2023-05-26 15:32:55 +0100

    javasolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,16 @@ for (var 1 = 1; i < 30; i++) {
 var anchor = document.querySelector("#main-navigation").querySelectorAll("a");
 
 for (var i = 0; i < anchor.length; i++) {
-  if(window.location.href === anchor[i].href) {
-    anchor[i].classList.add("active"); 
-  }
+    if (window.location.href === anchor[i].href) {
+        anchor[i].classList.add("active");
+    }
 
-  if (window.location.href.indexOf("portfolio") > -1) {
-    document.body.classList.add("portfolio");
-  } else if (window.location.href.indexOf("contacts") > -1) {
-    document.body.classList.add("contacts");
-  } else {
-    document.body.classList.add("home");
-  }
+    if (window.location.href.indexOf("portfolio") > -1) {
+        document.body.classList.add("portfolio");
+    } else if (window.location.href.indexOf("Contacts") > -1) {
+        document.body.classList.add("Contacts");
+    } else {
+        document.body.classList.add("home");
+    }
 
 }
